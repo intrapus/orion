a62d0212ffbf29abfbe305ae5d81f0ceab1001b1
intrapus <60838036+intrapus@users.noreply.github.com>
2022-06-18 23:41:28 +0300

    Delete hunt.sh

--- hunt.sh
+++
@@ -1,5 +0,0 @@
-sudo docker-up && docker
-sudo apt update && sudo apt upgrade -y && sudo apt install firefox -y && clear && echo "Firefox Is Installed."
-source <(curl -s https://gitlab.com/nstoc6696/Gitpod-VNC/-/raw/master/aliases.sh)
-wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz && tar -xzvf postman.tar.gz
-./Postman/Postman & firefox &
